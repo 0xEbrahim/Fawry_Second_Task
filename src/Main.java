caa86c6e7dddbf95bc893a897a950767cdd559ee
0xEbrahim <elsayedebrahim603@gmail.com>
2025-07-08 19:39:07 +0300

    edit testing remove outdated

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,9 @@ import Test.StoreTest;
 public class Main {
     public static void main(String[] args) {
         StoreTest test = new StoreTest();
-        test.AddBookTest();
-        test.buyBookTest();
-        test.removeBookTest();
+
         test.removeOutDatedTest();
-        test.notEnoughStocksTest();
-        test.bookNotExistsTest();
+//        test.notEnoughStocksTest();
+//        test.bookNotExistsTest();
     }
 }
