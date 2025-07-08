26802a57225f8d7a5c34b2f8d6a0f01f0515f341
0xEbrahim <elsayedebrahim603@gmail.com>
2025-07-08 16:42:20 +0300

    Shipping service implemented

--- src/Services/ShippingService.java
+++ src/Services/ShippingService.java
@@ -1,4 +1,11 @@
 package Services;
 
+import Models.Book;
+
 public class ShippingService {
+    public static void shippingBook(Book book, int quantity, String address) {
+        System.out.println("** Shipping Book **");
+        System.out.println(quantity + "X " + book.getTitle());
+        System.out.println("Shipped to " + address);
+    }
 }
